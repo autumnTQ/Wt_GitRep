445abfc1b2fead7f51559862a1213d24ce62b79e
wttttt <891570676@qq.com>
2024-03-06 09:10:39 +0800

    编译过程序

--- FreeRTOS/11. FreeRTOS事件标志组/SYSTEM/usart/usart.c
+++ FreeRTOS/11. FreeRTOS事件标志组/SYSTEM/usart/usart.c
@@ -67,20 +67,20 @@ void uart_init(u32 bound){
 	USART_InitTypeDef USART_InitStructure;
 	NVIC_InitTypeDef NVIC_InitStructure;
 	
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB,ENABLE); //使能GPIOA时钟
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE); //使能GPIOA时钟
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);//使能USART1时钟
  
 	//串口1对应引脚复用映射
-	GPIO_PinAFConfig(GPIOB,GPIO_PinSource6,GPIO_AF_USART1); //GPIOA9复用为USART1
-	GPIO_PinAFConfig(GPIOB,GPIO_PinSource7,GPIO_AF_USART1); //GPIOA10复用为USART1
+	GPIO_PinAFConfig(GPIOB,GPIO_PinSource9,GPIO_AF_USART1); //GPIOA9复用为USART1
+	GPIO_PinAFConfig(GPIOB,GPIO_PinSource10,GPIO_AF_USART1); //GPIOA10复用为USART1
 	
 	//USART1端口配置
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7; //GPIOA9与GPIOA10
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10; //GPIOA9与GPIOA10
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;                //复用功能
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	        //速度50MHz
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;              //推挽复用输出
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;        //上拉
-	GPIO_Init(GPIOB,&GPIO_InitStructure); //初始化PA9，PA10
+	GPIO_Init(GPIOA,&GPIO_InitStructure); //初始化PA9，PA10
 
    //USART1 初始化设置
 	USART_InitStructure.USART_BaudRate = bound;//波特率设置
